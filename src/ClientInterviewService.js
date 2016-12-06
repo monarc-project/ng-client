d3b41ea6b6956df3fd59a61acf731fa9b4d060a4
Guillaume Lesniak <guillaume@netlor.fr>
2016-12-06 11:41:02 +0100

    Add ClientInterviewService

---
+++ src/ClientInterviewService.js
@@ -0,0 +1,48 @@
+(function () {
+
+    angular
+        .module('ClientApp')
+        .factory('ClientInterviewService', [ '$resource', ClientInterviewService ]);
+
+    function ClientInterviewService($resource) {
+        var self = this;
+
+        self.ClientInterviewResource = $resource('/api/anr/:anr/interviews/:id', { 'id': '@id', 'anr': '@anr' }, {
+            'update': {
+                method: 'PATCH'
+            },
+            'query': {
+                isArray: false
+            }
+        });
+
+        var getInterviews = function (params) {
+            return self.ClientInterviewResource.query(params).$promise;
+        };
+
+        var getInterview = function (id) {
+            return self.ClientInterviewResource.query({id: id}).$promise;
+        };
+
+        var createInterview = function (params, success, error) {
+            new self.ClientInterviewResource(params).$save(success, error);
+        };
+
+        var updateInterview = function (params, success, error) {
+            self.ClientInterviewResource.update(params, success, error);
+        };
+
+        var deleteInterview = function (params, success, error) {
+            self.ClientInterviewResource.delete(params, success, error);
+        };
+        return {
+            getInterviews: getInterviews,
+            getInterview: getInterview,
+            createInterview: createInterview,
+            updateInterview: updateInterview,
+            deleteInterview: deleteInterview
+        };
+    }
+
+})
+();
