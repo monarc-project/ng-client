a39d4c0af0ff89bec0fbe5af8d49a11be9c8212f
Jerome Lombardi <jerome.lombardi@cases.lu>
2018-01-03 13:19:41 +0100

    improve the test

--- src/ClientDashboardCtrl.js
+++ src/ClientDashboardCtrl.js
@@ -38,10 +38,10 @@
                },
                duration: 500,
                xAxis: {
-                   axisLabel: 'X Axis'
+                   axisLabel: ''
                },
                yAxis: {
-                   axisLabel: 'Y Axis',
+                   axisLabel: gettextCatalog.getString('Current risk'),
                    axisLabelDistance: -10
                }
            }
@@ -65,7 +65,7 @@
                 staggerLabels : false,
                 wrapLabels : false,
                 xAxis: {
-                    axisLabel: 'Time (ms)',
+                    axisLabel: gettextCatalog.getString('Asset'),
                     showMaxMin: false,
                     rotateLabels : 90,
                     height : 150,
@@ -74,7 +74,7 @@
                     }
                 },
                 yAxis: {
-                    axisLabel: 'Y Axis',
+                    axisLabel: gettextCatalog.getString('Current risk'),
                     axisLabelDistance: -20,
                     tickFormat: function(d){
                         return d3.format(',.1f')(d);
@@ -165,11 +165,14 @@
               tab[i].y++;
         }
         var updateChartRisks = function (anrId) {
-          //fetch data : TODO : fetch all risks (see ApiAnrRisksController.php)
-          // TODO : make the function multinlingual
           // TODO : Finaly clean the code and create an api who send the information in the right format, maybe better ?
-          // TODO : fetch the right value for the "seuil"
-            $http.get("api/client-anr/" + anrId + "/risks").then(function (data) {
+          treshold1 = 0;
+          treshold2 = 0;
+          $http.get("api/client-anr/" + anrId).then(function (data) {
+            treshold1 = data.data.seuil1;
+            treshold2 = data.data.seuil2;
+
+            $http.get("api/client-anr/" + anrId + "/risks?limit=-1").then(function (data) {
               risksList = data.data.risks;
               for (var i=0; i < risksList.length ; ++i)
               {
@@ -180,7 +183,7 @@
                   {
                     // initialize element
                     eltlow.id = eltmed.id = elthigh.id = risksList[i].instance; //keep the instance id as id
-                    eltlow.x = eltmed.x = elthigh.x = risksList[i].instanceName1;
+                    eltlow.x = eltmed.x = elthigh.x = $scope._langField(risksList[i],'instanceName');
                     eltlow.y = eltmed.y = elthigh.y = 0;
                     eltlow.color = '#D6F107';
                     $scope.dataChartRisks[0].values.push(eltlow);
@@ -189,21 +192,22 @@
                     elthigh.color = '#FD661F';
                     $scope.dataChartRisks[2].values.push(elthigh);
                   }
-                  if(risksList[i].max_risk>26)
+                  if(risksList[i].max_risk>treshold2)
                   {
                     addOneRisk($scope.dataChartRisks[2].values,risksList[i].instance);
                   }
-                  else if (risksList[i].max_risk<27 && risksList[i].max_risk>8)
+                  else if (risksList[i].max_risk<=treshold2 && risksList[i].max_risk>treshold1)
                   {
                     addOneRisk($scope.dataChartRisks[1].values,risksList[i].instance);
                   }
-                  else if (risksList[i].max_risk>-1 && risksList[i].max_risk<9)
+                  else if (risksList[i].max_risk>-1 && risksList[i].max_risk<=treshold1)
                   {
                     addOneRisk($scope.dataChartRisks[0].values,risksList[i].instance);
                   }
                 }
               }
             );
+          });
         };
 
         var updateCartoRisks = function (anrId) {
@@ -220,8 +224,6 @@
                   $scope.dataCartoRisk[0].values[1].value = data.data.carto.real.distrib[1];
                   $scope.dataCartoRisk[0].values[2].label = gettextCatalog.getString('high risks');
                   $scope.dataCartoRisk[0].values[2].value = data.data.carto.real.distrib[2];
-
-
 
                 for (var i = 0; i < 3; ++i) {
                     if ($scope.dashboard.carto.real.distrib[i] && !isNaN($scope.dashboard.carto.real.distrib[i])) {
