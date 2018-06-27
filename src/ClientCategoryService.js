7fdab0611dee25d3b006fc76a9100592c51a69b4
guelzim <guelzimj@gmail.com>
2018-06-27 15:53:58 +0200

    add clientCategoryService

--- src/ClientCategoryService.js
+++ src/ClientCategoryService.js
@@ -27,10 +27,23 @@
                     return self.ClientCategoryResource.query({'anr': params.anr, 'id': params.id}).$promise;
                 };
 
+                var createCategory = function (params, success, error) {
+                    new self.CategoryResource(params).$save(success, error);
+                };
 
+                var updateCategory = function (params, success, error) {
+                    self.CategoryResource.update(params, success, error);
+                };
+
+                var deleteCategory = function (id, success, error) {
+                    self.CategoryResource.delete({categoryId: id}, success, error);
+                };
                 return {
                     getCategories: getCategories,
                     getCategory: getCategory,
+                    createCategory: createCategory,
+                    deleteCategory: deleteCategory,
+                    updateCategory: updateCategory
 
                 };
 
