f8e22cc1c8b40b181ddbe5b5e582a521cd3a08a2
Juan Rocha <juan.rocha@securitymadein.lu>
2018-12-14 13:29:43 +0100

    Revert "Add service to reload ANR ID"
    This reverts commit 39428998c2e125069e42419834238eb37fceb4c6.

--- src/ClientApp.js
+++ src/ClientApp.js
@@ -412,10 +412,7 @@ angular
         var lastKnownAnrId;
         $rootScope.$on('$locationChangeStart', function () {
             if ($rootScope.getUrlAnrId() != lastKnownAnrId) {
-                var services = ['AmvService', 'AssetService', 'CategoryService', 'MeasureService',
-                                'ObjlibService', 'RiskService', 'TagService', 'ThreatService',
-                                'VulnService', 'ClientSnapshotService', 'QuestionService',
-                                'ReferentialService', 'MeasureMeasureService', 'SOACategoryService'];
+                var services = ['AmvService', 'AssetService', 'CategoryService', 'MeasureService', 'ObjlibService', 'RiskService', 'TagService', 'ThreatService', 'VulnService', 'ClientSnapshotService', 'QuestionService'];
                 for (var i = 0; i < services.length; ++i) {
                     $injector.get(services[i]).makeResource();
                 }
