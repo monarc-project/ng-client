04d980029ae56b611b16707169d832c768e12c1f
Juan Rocha <juan.rocha@securitymadein.lu>
2019-02-26 16:25:12 +0100

    Add compliance chart in Dashboard

--- src/ClientDashboardCtrl.js
+++ src/ClientDashboardCtrl.js
@@ -4,15 +4,16 @@
         .module('ClientApp')
         .controller('ClientDashboardCtrl', [
             '$scope', '$state', '$http', 'gettextCatalog', 'toastr', '$rootScope', '$timeout',
-            '$stateParams', 'AnrService', 'ClientAnrService',
-            ClientDashboardCtrl
+            '$stateParams', 'AnrService', 'ClientAnrService', 'ReferentialService', 'SOACategoryService',
+            'ClientSoaService', ClientDashboardCtrl
         ]);
 
     /**
      * Dashboard Controller for the Client module
      */
     function ClientDashboardCtrl($scope, $state, $http, gettextCatalog, toastr, $rootScope, $timeout,
-                                 $stateParams, AnrService, ClientAnrService) {
+                                 $stateParams, AnrService, ClientAnrService, ReferentialService, SOACategoryService,
+                                 ClientSoaService) {
 
         $scope.dashboard = {
             anr: null,
@@ -58,8 +59,8 @@
             loadGraph($scope.graphCartography,optionsChartCartography,dataChartCartography);
         };
 
-        $scope.selectGraphDecisionSupport = function () { //Displays the decision support tab
-
+        $scope.selectGraphCompliance = function () { //Displays the Compliance tab
+          //RadarChart('#graphCompliance', optionsChartCompliance, dataChartCompliance[$scope.dashboard.refSelected]);
         };
 
         $scope.selectGraphPerspective = function () { //Displays the persepctive charts
@@ -653,6 +654,26 @@
         }
     }
 
+//==============================================================================
+
+    //Options for the chart that displays the compliance
+    optionsChartCompliance = {
+       radius: 5,
+       w: 650,
+       h: 650,
+       factor: 1,
+       factorLegend: 1.1,
+       levels: 5,
+       maxValue: 1,
+       radians: 2 * Math.PI,
+       opacityArea: 0.5,
+       ToRight: 5,
+       TranslateX: 200,
+       TranslateY: 30,
+       ExtraWidthX: 500,
+       ExtraWidthY: 150,
+       color: d3.scale.category10()
+    };
 
 // DATA MODELS =================================================================
 
@@ -806,6 +827,9 @@
         //Data for the graph for the vulnerabilities by vulnerabilities risk
         dataChartCartography = [];
 
+        //Data for the graph for the compliance
+        dataChartCompliance = [];
+
 //==============================================================================
 
         /*
@@ -883,6 +907,20 @@
                         if ($scope.dashboard.currentTabIndex == 3) {
                           $scope.selectGraphCartography();
                         }
+                        ReferentialService.getReferentials({order: 'createdAt'}).then(function (data) {
+                          $scope.dashboard.referentials = data['referentials'];
+
+                          SOACategoryService.getCategories().then(function (data) {
+                            $scope.dashboard.categories = data['categories'];
+                            ClientSoaService.getSoas().then(function (data) {
+                              $scope.dashboard.soa = data['soaMeasures'];
+                              updateCompliance($scope.dashboard.referentials, $scope.dashboard.categories,$scope.dashboard.soa);
+                              if ($scope.dashboard.referentials[0]) {
+                                $scope.dashboard.refSelected = $scope.dashboard.referentials[0].uuid;
+                              }
+                            });
+                          });
+                        });
                         $scope.firstRefresh = false;
                     });
                  });
@@ -943,7 +981,7 @@
         /*
         * Generate the excel with the DATAs of all the graphs of Dashboard
         */
-         $scope.generateXlsxData = async function (){
+         $scope.generateXlsxData = function (){
           //prepare by risk level
            var byLevel = dataChartCurrentRisksByLevel_discreteBarChart[0].values.map(({label,value}) => ({label,value}));
            byLevel.forEach(function(obj){
@@ -1053,7 +1091,7 @@
             }
         });
 
-        $scope.$watch('displayThreatsBy', function (newValue, oldValue) {
+        $scope.$watch('displayThreatsBy', function (newValue) {
           if ($scope.dashboard.data.count) {
             updateThreats($scope.dashboard.data);
           }
@@ -1089,6 +1127,12 @@
                 loadGraph($scope.graphCartography, optionsChartCartography, dataChartCartography);
             }
           }
+        });
+
+        $scope.$watch('dashboard.refSelected', function (newValue) {
+            if (newValue){
+              RadarChart('#graphCompliance', optionsChartCompliance, dataChartCompliance[$scope.dashboard.refSelected]);
+            }
         });
 
 //==============================================================================
@@ -1912,6 +1956,217 @@
           }
         };
 
-    }
+//==============================================================================
 
+        /*
+        * Update the data for the compliance
+        */
+        var updateCompliance = function (referentials,categories,data){
+          referentials.forEach(function(ref){
+            dataChartCompliance[ref.uuid] = [[]];
+            categories.filter(category => category.referential.uuid == ref.uuid).forEach(function(cat){
+              let catData = {
+                axis:cat['label'+ $scope.dashboard.anr.language],
+                id:cat.id,
+                value: null
+              }
+              let complianceValues = data.filter(soa => soa.measure.category.id == cat.id).map(soa => soa.compliance);
+              let sum = complianceValues.reduce(function(a, b) { return a + b; }, 0);
+              let avg = (sum / complianceValues.length) * 0.2;
+              catData.value = avg.toFixed(2);
+              dataChartCompliance[ref.uuid][0].push(catData);
+            })
+          });
+        }
+
+//==============================================================================
+
+        /*
+        * Generate Radar Chart
+        */
+        function RadarChart(id, cfg, d){
+        	cfg.maxValue = Math.max(cfg.maxValue, d3.max(d, function(i){return d3.max(i.map(function(o){return o.value;}))}));
+        	var allAxis = (d[0].map(function(i, j){return i.axis}));
+        	var total = allAxis.length;
+        	var radius = cfg.factor*Math.min(cfg.w/2, cfg.h/2);
+        	var Format = d3.format('%');
+        	d3.select(id).select("svg").remove();
+
+        	var g = d3.select(id)
+        			.append("svg")
+        			.attr("width", cfg.w+cfg.ExtraWidthX)
+        			.attr("height", cfg.h+cfg.ExtraWidthY)
+        			.append("g")
+        			.attr("transform", "translate(" + cfg.TranslateX + "," + cfg.TranslateY + ")");
+        			;
+        	var tooltip;
+
+        	//Circular segments
+        	for(var j=0; j<cfg.levels; j++){
+        	  var levelFactor = cfg.factor*radius*((j+1)/cfg.levels);
+        	  g.selectAll(".levels")
+        	   .data(allAxis)
+        	   .enter()
+        	   .append("svg:line")
+        	   .attr("x1", function(d, i){return levelFactor*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
+        	   .attr("y1", function(d, i){return levelFactor*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
+        	   .attr("x2", function(d, i){return levelFactor*(1-cfg.factor*Math.sin((i+1)*cfg.radians/total));})
+        	   .attr("y2", function(d, i){return levelFactor*(1-cfg.factor*Math.cos((i+1)*cfg.radians/total));})
+        	   .attr("class", "line")
+        	   .style("stroke", "grey")
+        	   .style("stroke-opacity", "0.75")
+        	   .style("stroke-width", "0.3px")
+        	   .attr("transform", "translate(" + (cfg.w/2-levelFactor) + ", " + (cfg.h/2-levelFactor) + ")");
+        	}
+
+        	//Text indicating at what % each level is
+        	for(var j=0; j<cfg.levels; j++){
+        	  var levelFactor = cfg.factor*radius*((j+1)/cfg.levels);
+        	  g.selectAll(".levels")
+        	   .data([1]) //dummy data
+        	   .enter()
+        	   .append("svg:text")
+        	   .attr("x", function(d){return levelFactor*(1-cfg.factor*Math.sin(0));})
+        	   .attr("y", function(d){return levelFactor*(1-cfg.factor*Math.cos(0));})
+        	   .attr("class", "legend")
+        	   .style("font-family", "sans-serif")
+        	   .style("font-size", "10px")
+        	   .attr("transform", "translate(" + (cfg.w/2-levelFactor + cfg.ToRight) + ", " + (cfg.h/2-levelFactor) + ")")
+        	   .attr("fill", "#737373")
+        	   .text(Format((j+1)*cfg.maxValue/cfg.levels));
+        	}
+
+        	series = 0;
+
+        	var axis = g.selectAll(".axis")
+        			.data(allAxis)
+        			.enter()
+        			.append("g")
+        			.attr("class", "axis");
+
+        	axis.append("line")
+        		.attr("x1", cfg.w/2)
+        		.attr("y1", cfg.h/2)
+        		.attr("x2", function(d, i){return cfg.w/2*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
+        		.attr("y2", function(d, i){return cfg.h/2*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
+        		.attr("class", "line")
+        		.style("stroke", "grey")
+        		.style("stroke-width", "1px");
+
+        	axis.append("text")
+        		.attr("class", "legend")
+        		.text(function(d){return d})
+        		.style("font-family", "sans-serif")
+        		.style("font-size", "11px")
+        		.attr("text-anchor", "middle")
+        		.attr("dy", "1.5em")
+        		.attr("transform", function(d, i){return "translate(0, -10)"})
+        		.attr("x", function(d, i){return cfg.w/2*(1-cfg.factorLegend*Math.sin(i*cfg.radians/total))-60*Math.sin(i*cfg.radians/total);})
+        		.attr("y", function(d, i){return cfg.h/2*(1-Math.cos(i*cfg.radians/total))-20*Math.cos(i*cfg.radians/total);});
+
+
+        	d.forEach(function(y, x){
+        	  dataValues = [];
+        	  g.selectAll(".nodes")
+        		.data(y, function(j, i){
+        		  dataValues.push([
+        			cfg.w/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total)),
+        			cfg.h/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total))
+        		  ]);
+        		});
+        	  dataValues.push(dataValues[0]);
+
+        	  g.selectAll(".area")
+        					 .data([dataValues])
+        					 .enter()
+        					 .append("polygon")
+        					 .attr("class", "radar-chart-serie"+series)
+        					 .style("stroke-width", "2px")
+        					 .style("stroke", cfg.color(series))
+        					 .attr("points",function(d) {
+        						 var str="";
+        						 for(var pti=0;pti<d.length;pti++){
+        							 str=str+d[pti][0]+","+d[pti][1]+" ";
+        						 }
+        						 return str;
+        					  })
+        					 .style("fill", function(j, i){return cfg.color(series)})
+        					 .style("fill-opacity", cfg.opacityArea)
+        					 .on('mouseover', function (d){
+        										z = "polygon."+d3.select(this).attr("class");
+        										g.selectAll("polygon")
+        										 .transition(200)
+        										 .style("fill-opacity", 0.1);
+        										g.selectAll(z)
+        										 .transition(200)
+        										 .style("fill-opacity", .7);
+        									  })
+        					 .on('mouseout', function(){
+        										g.selectAll("polygon")
+        										 .transition(200)
+        										 .style("fill-opacity", cfg.opacityArea);
+        					 });
+        	  series++;
+        	});
+        	series=0;
+
+
+        	d.forEach(function(y, x){
+        	  g.selectAll(".nodes")
+        		.data(y).enter()
+        		.append("svg:circle")
+        		.attr("class", "radar-chart-serie"+series)
+        		.attr('r', cfg.radius)
+        		.attr("alt", function(j){return Math.max(j.value, 0)})
+        		.attr("cx", function(j, i){
+        		  dataValues.push([
+        			cfg.w/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total)),
+        			cfg.h/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total))
+        		]);
+        		return cfg.w/2*(1-(Math.max(j.value, 0)/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total));
+        		})
+        		.attr("cy", function(j, i){
+        		  return cfg.h/2*(1-(Math.max(j.value, 0)/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total));
+        		})
+        		.attr("data-id", function(j){return j.axis})
+        		.style("fill", cfg.color(series)).style("fill-opacity", .9)
+        		.on('mouseover', function (d){
+        					newX =  parseFloat(d3.select(this).attr('cx')) - 10;
+        					newY =  parseFloat(d3.select(this).attr('cy')) - 5;
+
+        					tooltip
+        						.attr('x', newX)
+        						.attr('y', newY)
+        						.text(Format(d.value))
+        						.transition(200)
+        						.style('opacity', 1);
+
+        					z = "polygon."+d3.select(this).attr("class");
+        					g.selectAll("polygon")
+        						.transition(200)
+        						.style("fill-opacity", 0.1);
+        					g.selectAll(z)
+        						.transition(200)
+        						.style("fill-opacity", .7);
+        				  })
+        		.on('mouseout', function(){
+        					tooltip
+        						.transition(200)
+        						.style('opacity', 0);
+        					g.selectAll("polygon")
+        						.transition(200)
+        						.style("fill-opacity", cfg.opacityArea);
+        				  })
+        		.append("svg:title")
+        		.text(function(j){return Math.max(j.value, 0)});
+
+        	  series++;
+        	});
+        	//Tooltip
+          	tooltip = g.append('text')
+          			   .style('opacity', 0)
+          			   .style('font-family', 'sans-serif')
+          			   .style('font-size', '13px');
+        };
+    }
 })();
