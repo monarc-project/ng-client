7ea2949c3a31f25881b7ff28ba64037d80b6cc02
Jerome Lombardi <jerome.lombardi@cases.lu>
2020-04-23 14:36:16 +0200

    First commit for generic groupped bar chart

--- src/ClientMainCtrl.js
+++ src/ClientMainCtrl.js
@@ -163,6 +163,204 @@
         };
 
         updateMenuANRs();
+
+        /**
+        * Exemple Data
+        */
+
+        dataSample =
+        [
+          {
+              "categorie": "Analysis 1",
+              "values": [
+                  {
+                      "value": 1,
+                      "rate": "High"
+                  },
+                  {
+                      "value": 4,
+                      "rate": "Medium"
+                  },
+                  {
+                      "value": 12,
+                      "rate": "Low"
+                  }
+              ]
+          },
+          {
+              "categorie": "Analysis 2",
+              "values": [
+                  {
+                      "value": 1,
+                      "rate": "High"
+                  },
+                  {
+                      "value": 21,
+                      "rate": "Medium"
+                  },
+                  {
+                      "value": 13,
+                      "rate": "Low"
+                  }
+              ]
+          },
+          {
+              "categorie": "Analysis 3",
+              "values": [
+                  {
+                      "value": 3,
+                      "rate": "High"
+                  },
+                  {
+                      "value": 22,
+                      "rate": "Medium"
+                  },
+                  {
+                      "value": 6,
+                      "rate": "Low"
+                  }
+              ]
+          }
+      ];
+
+
+        /***
+        * GRAPH PARTS
+        */
+
+
+        /*
+        * Generate a grouppedBarChart
+        * @param tag : string  : tag where to put the svg
+        * @param dataForTheGraph : JSON  : The data for the graph
+        * @param parameters : margin : {top: 20, right: 20, bottom: 30, left: 40}
+        *                     width : int : width of the graph
+        *                     height : int of the graph
+        *
+        */
+
+        function grouppedBarChart(tag,
+                                  dataForTheGraph,
+                                  parameters
+        ){
+          options = {'margin' : {top: 0, right: 0, bottom: 0, left: 0},
+            'width' : 960,
+            'height' : 500,
+            'barColor' : ["#FD661F","#FFBC1C","#D6F107"]
+          } //default options for the graph
+          options=$.extend(options,parameters); //merge the parameters to the default options
+
+          var margin = options['margin'];
+          width = options['width']- margin.left - margin.right;
+          height = options['height'] - margin.top - margin.bottom;
+
+          var x0 = d3.scale.ordinal()
+              .rangeRoundBands([0, width], .1);
+
+          var x1 = d3.scale.ordinal();
+
+          var y = d3.scale.linear()
+              .range([height, 0]);
+
+          var xAxis = d3.svg.axis()
+              .scale(x0)
+              .tickSize(0)
+              .orient("bottom");
+
+          var yAxis = d3.svg.axis()
+              .scale(y)
+              .orient("left");
+
+          var color = d3.scale.ordinal()
+              .range(options['barColor']);
+
+            var svg = d3.select(tag).append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            var categoriesNames = dataForTheGraph.map(function(d) { return d.categorie; });
+            var rateNames = dataForTheGraph[0].values.map(function(d) { return d.rate; });
+
+            x0.domain(categoriesNames);
+            x1.domain(rateNames).rangeRoundBands([0, x0.rangeBand()]);
+            y.domain([0, d3.max(dataForTheGraph, function(categorie) { return d3.max(categorie.values, function(d) { return d.value; }); })]);
+
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .style('opacity','0')
+                .call(yAxis)
+            .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .style('font-weight','bold')
+                .text("Value");
+
+            svg.select('.y').transition().duration(500).delay(1300).style('opacity','1');
+
+            var slice = svg.selectAll(".slice")
+                .data(dataForTheGraph)
+                .enter().append("g")
+                .attr("class", "g")
+                .attr("transform",function(d) { return "translate(" + x0(d.categorie) + ",0)"; });
+
+            slice.selectAll("rect")
+                .data(function(d) { return d.values; })
+            .enter().append("rect")
+                .attr("width", x1.rangeBand())
+                .attr("x", function(d) { return x1(d.rate); })
+                .style("fill", function(d) { return color(d.rate) })
+                .attr("y", function(d) { return y(0); })
+                .attr("height", function(d) { return height - y(0); })
+                .on("mouseover", function(d) {
+                    d3.select(this).style("fill", d3.rgb(color(d.rate)).darker(2));
+                })
+                .on("mouseout", function(d) {
+                    d3.select(this).style("fill", color(d.rate));
+                });
+
+            slice.selectAll("rect")
+                .transition()
+                .delay(function (d) {return Math.random()*1000;})
+                .duration(1000)
+                .attr("y", function(d) { return y(d.value); })
+                .attr("height", function(d) { return height - y(d.value); });
+
+            //Legend
+            var legend = svg.selectAll(".legend")
+                .data(dataForTheGraph[0].values.map(function(d) { return d.rate; }).reverse())
+            .enter().append("g")
+                .attr("class", "legend")
+                .attr("transform", function(d,i) { return "translate(0," + i * 20 + ")"; })
+                .style("opacity","0");
+
+            legend.append("rect")
+                .attr("x", width - 18)
+                .attr("width", 18)
+                .attr("height", 18)
+                .style("fill", function(d) { return color(d); });
+
+            legend.append("text")
+                .attr("x", width - 24)
+                .attr("y", 9)
+                .attr("dy", ".35em")
+                .style("text-anchor", "end")
+                .text(function(d) {return d; });
+
+            legend.transition().duration(500).delay(function(d,i){ return 1300 + 100 * i; }).style("opacity","1");
+          }
+
+        options = {'width':500,'height':500}
+        grouppedBarChart('body',dataSample,options);
+
     }
 
 })();
