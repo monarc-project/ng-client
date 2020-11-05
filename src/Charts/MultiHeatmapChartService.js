0a4e5549d0b16eeba8a3f3168e4b8cdd732b378d
Juan Rocha <juan.rocha@securitymadein.lu>
2020-11-05 13:07:02 +0100

    Improved multiHeatmap chart

--- src/Charts/MultiHeatmapChartService.js
+++ src/Charts/MultiHeatmapChartService.js
@@ -4,58 +4,58 @@
     .module('ClientApp')
     .factory('MultiHeatmapChartService', ['gettextCatalog', function (gettextCatalog){
 
-      /*
-      * Generate a grouped/stacked Vertical Bar Chart
-      * @param tag : string  : tag where to put the svg
-      * @param data : JSON  : The data for the graph
-      * @param parameters : margin : {top: 20, right: 20, bottom: 30, left: 40}
-      *                     width : int : width of the graph
-      *                     color : array : transition between colors
-      *        {int}        legendSize - width of the graph for the legend
-      *                     threshold : array : threshold values related to color array
-      *                     xLabel : string : x axis label
-      *                     yLabel : string : y axis label
-      *
-      */
+      /**
+       * Generate a Multi Heatmap Chart
+       * @param {string} tag - tag where to put the svg
+       * @param {object} data - The data for the graph
+       * @param {object} parameters - options of chart
+       *        {object}   margin - top: 20, right: 20, bottom: 30, left: 40
+       *        {int}      width - width of the graph
+       *        {array}    color - colors pallete of series
+       *        {array}    threshold - threshold values related to color array
+       *        {string}   xLabel - x axis label
+       *        {string}   yLabel - y axis label
+       *        {int}      columnsLegend - number of Legend columns
+       * @return {svg} chart svg
+       */
 
       function draw(tag, data, parameters){
         var options = {
-          margin : {top: 50, right: 50, bottom: 30, left: 40},
-          width : 500,
-          legendSize : 250,
+          margin : {top: 50, right: 30, bottom: 50, left: 40},
+          width : 400,
           color : d3.schemeCategory10,
-          axisYvalues : [0,1,2,3,4],
-          axisXvalues : [0,1,2,3,4,5,6,8,9,10,12,15,16,20]
+          columnsLegend : 3,
         } //default options for the graph
 
         options=$.extend(options,parameters); //merge the parameters to the default options
 
         var margin = options.margin,
-            width = options.width - margin.left - margin.right - options.legendSize;
+            width = options.width - margin.left - margin.right;
 
+        var categoriesUuids = [];
+        var dataLegend = [];
         var seriesMerged = mergeData(data);
-        var categoriesUuids = data.map(d => d.uuid);
         var maxValue = d3.max(seriesMerged.map(d => d.value));
-        var xTicks = [...new Set(seriesMerged.map(d => d.x))];
-        var yTicks = [...new Set(seriesMerged.map(d => d.y))];
+        var xTicks = [...new Set(seriesMerged.map(d => d.x))].sort((a,b) => a - b);
+        var yTicks = [...new Set(seriesMerged.map(d => d.y))].sort((a,b) => a - b);
         var gridSize = width / xTicks.length;
         var height = gridSize * yTicks.length;
         var cellCoords = [];
 
-        options.axisYvalues.forEach(yCoord => {
-          options.axisXvalues.forEach(xCoord =>{
+        yTicks.forEach(yCoord => {
+          xTicks.forEach(xCoord =>{
             cellCoords.push({y:yCoord,x:xCoord})
           })
         });
 
         d3.select(tag).select("svg").remove();
 
-        if (categoriesUuids.length * 20 > height + margin.bottom) {
-          margin.bottom = (categoriesUuids.length * 20) - height;
+        if (Math.floor((categoriesUuids.length/options.columnsLegend) + 1) * 25 > margin.bottom) {
+          margin.bottom = ((Math.floor(categoriesUuids.length/options.columnsLegend) + 1) * 25) + 20;
         }
 
         var svg = d3.select(tag).append("svg")
-              .attr("width", width + margin.left + margin.right + options.legendSize)
+              .attr("width", width + margin.left + margin.right)
               .attr("height", height + margin.top + margin.bottom)
               .style("user-select","none")
             .append("g")
@@ -139,7 +139,7 @@
         }
 
         var cellsGroup = svg.append('g')
-        .attr("class",'cells')
+        .attr("class",'cells');
 
         var cells = cellsGroup.selectAll('cell')
             .data(cellCoords);
@@ -156,156 +156,173 @@
               return color(d.value);
             }
           })
-          .style("fill-opacity", 0.2)
+          .style("fill-opacity", 0.2);
 
-          var circlesGroup = svg.append('g')
-          .attr("class",'circles')
+        var circlesGroup = svg.append('g')
+        .attr("class",'circles');
 
-          var circles = circlesGroup.selectAll('circle')
-          .data(seriesMerged);
+        var circles = circlesGroup.selectAll('circle')
+        .data(seriesMerged);
 
-          circles.enter().append("circle")
-            .attr("transform", d => { return `translate(${x(d.x)},${y(d.y)})`})
-            .attr("cx", gridSize/2)
-            .attr("cy", gridSize/2)
-            .attr("r", d => gridSize/2 * (d.value /maxValue))
-            .style("fill", d => color(d.x * d.y))
-            .style("fill-opacity", 0.35)
-            .style("stroke", d => color(d.x * d.y))
-            .style("stroke-width", 1)
-            .on("mouseover", function() { mouseover() })
-            .on("mousemove", function(d) { mousemove(d,this) })
-            .on("mouseleave", function() { mouseleave() });
+        circles.enter().append("circle")
+          .attr("transform", d => { return `translate(${x(d.x)},${y(d.y)})`})
+          .attr("cx", gridSize/2)
+          .attr("cy", gridSize/2)
+          .attr("r", d => gridSize/2 * (d.value /maxValue))
+          .style("fill", d => color(d.x * d.y))
+          .style("fill-opacity", 0.35)
+          .style("stroke", d => color(d.x * d.y))
+          .style("stroke-width", 1)
+          .on("mouseover", function() { mouseover() })
+          .on("mousemove", function(d) { mousemove(d,this) })
+          .on("mouseleave", function() { mouseleave() });
 
-            var legend = svg.selectAll(".legend")
-                  .data(data)
-                .enter().append('g')
-                  .attr("class", "legend")
-                  .attr("transform", (d,i) => `translate(0,${i * 20})`)
-
-            legend.append("rect")
-                  .attr("x", width + 20)
-                  .attr("width", 18)
-                  .attr("height", 18)
-                  .style("fill", 'none')
-                  .style("stroke", 'black')
-
-            legend.append("rect")
-                  .attr("class","checkbox")
-                  .attr("x", width + 20)
-                  .attr("transform", 'translate(3,3)')
-                  .attr("width", 12)
-                  .attr("height", 12)
-                  .style("fill-opacity", 0.6)
-                  .on('click', function(d){ updateChart(this,d) });
-
-
-            legend.append("text")
-                  .attr("x", width + 45)
-                  .attr("y", 12)
-                  .style("font-size",10)
-                  .attr("height",30)
-                  .attr("width",100)
-                  .text(d => d.category);
-
-
-            function mergeData(data) {
-              let dataMerged = []
-
-              data.forEach((category) => {
-                category.series.forEach(d => {
-                  let serieFound = dataMerged.findIndex(function(serie) {
-                    return serie.y == d.y && serie.x == d.x
-                  });
-                  if (serieFound == -1) {
-                    dataMerged.push({
-                      y: d.y,
-                      x: d.x,
-                      category: [{
-                        name: category.category,
-                        uuid: category.uuid,
-                        value: d.value
-                      }],
-                      value: d.value,
-                    })
-                  } else if (d.value !== null) {
-                    dataMerged[serieFound].category.push({
-                      name: category.category,
-                      uuid: category.uuid,
-                      value: d.value
-                    });
-                    dataMerged[serieFound].value += d.value;
-                  }
-                })
+        var legend = svg.selectAll(".legend")
+              .data(dataLegend)
+            .enter().append('g')
+              .attr("class", "legend")
+              .attr("transform", (d,i) => {
+                let row = Math.floor(i/options.columnsLegend);
+                let x = i * width/options.columnsLegend - row * width;
+                let y = row * 25;
+                return `translate(${x},${y})`
               });
 
-              return dataMerged;
-            }
+        legend.append("circle")
+              .attr("cy", height + 29)
+              .attr("cx", 9)
+              .attr("r", 9)
+              .style("fill", 'none')
+              .style("stroke", 'rgb(0,111,186)');
 
-            function updateChart(element,d) {
-              let checkbox = d3.select(element);
-              let uuid = d.uuid;
-              if (checkbox.style("fill-opacity") == 0.6) {
-                checkbox.style("fill-opacity",0)
-                categoriesUuids.splice(categoriesUuids.indexOf(uuid), 1);
-              }else{
-                checkbox.style("fill-opacity",0.6)
-                categoriesUuids.push(uuid);
-              }
-              if (categoriesUuids.length < 1) {
-                categoriesUuids = data.map(d => d.uuid);
-                svg.selectAll('.checkbox').style("fill-opacity",0.6);
-              }
+        legend.append("circle")
+              .attr("class","checkbox")
+              .attr("cy", height + 29)
+              .attr("cx", 9)
+              .attr("r", 6)
+              .style("fill","rgb(0,111,186)")
+              .style("fill-opacity", 0.87)
+              .on('click', function(d){ updateChart(this,d) });
 
-              let newData = data.filter((d) => {
-                  return categoriesUuids.indexOf(d.uuid) > -1;
-                })
+        legend.append("text")
+              .attr("y", height + 32)
+              .attr("x", 25)
+              .style("font-size",10)
+              .text(d => {
+                let ratioCharPixels = (width - (25 * options.columnsLegend))/(options.columnsLegend * 6.5);
+                if (d.label.length > ratioCharPixels) {
+                  return d.category.substring(0, ratioCharPixels) +'...';
+                }else{
+                  return d.label;
+                }
+              });
 
-              seriesMerged = mergeData(newData);
+        function mergeData(data) {
+          let dataMerged = []
+          categoriesUuids = [];
+          dataLegend = [];
 
-              maxValue = d3.max(seriesMerged.map(d => d.value));
-
-              let newCircles = svg.selectAll('.circles').selectAll('circle')
-              .data(seriesMerged);
-
-              newCircles
-                .attr("r", d => gridSize/2 * (d.value /maxValue))
-                .transition().duration(500);
-
-              newCircles.exit().remove();
-
-            }
-
-            function mouseover() {
-              tooltip
-                .style("z-index", "100")
-                .style("opacity", 0.9);
-            }
-
-            function mousemove(d, element) {
-              let elementRect = element.getBoundingClientRect();
-              let tooltipText = "<tr><td><b>"+ gettextCatalog.getString("Total") +
-                                "</td><td><b>"+ d.value + "</td></tr><tr><td></td></tr>"
-              d.category.forEach(function(category){
-                  if (category.value) {
-                    tooltipText =
-                            tooltipText +
-                            ("<tr><td>"+ category.name +
-                            "</td><td><b>"+ category.value + "</td></tr>");
-                  }
+          data.forEach((category) => {
+            let seriesValues = category.series.map(serie => serie.value);
+            if (!seriesValues.every(value => value === null)) {
+              categoriesUuids.push(category.uuid);
+              dataLegend.push({
+                label:category.category,
+                uuid:category.uuid
+              });
+              category.series.forEach(d => {
+                let serieFound = dataMerged.findIndex(function(serie) {
+                  return serie.y == d.y && serie.x == d.x
+                });
+                if (serieFound == -1) {
+                  dataMerged.push({
+                    y: d.y,
+                    x: d.x,
+                    category: [{
+                      label: category.category,
+                      uuid: category.uuid,
+                      value: d.value
+                    }],
+                    value: d.value,
+                  })
+                } else if (d.value !== null) {
+                  dataMerged[serieFound].category.push({
+                    label: category.category,
+                    uuid: category.uuid,
+                    value: d.value
+                  });
+                  dataMerged[serieFound].value += d.value;
+                }
               })
-              tooltip
-                .html("<table><tbody>"+ tooltipText + "</tbody></table>")
-                .style("left", elementRect.right + 10 + "px")
-                .style("top", elementRect.top + (elementRect.height/2) - tooltip.property('clientHeight')/2 + "px")
             }
+          });
 
-            function mouseleave() {
-              tooltip
-                .style("z-index", "-100")
-                .style("opacity", 0)
-            }
+          return dataMerged;
+        }
 
+        function updateChart(element,d) {
+          let checkbox = d3.select(element);
+          let uuid = d.uuid;
+          if (checkbox.style("fill-opacity") == 0.87) {
+            checkbox.style("fill-opacity",0)
+            categoriesUuids.splice(categoriesUuids.indexOf(uuid), 1);
+          }else{
+            checkbox.style("fill-opacity",0.87)
+            categoriesUuids.push(uuid);
+          }
+          if (categoriesUuids.length < 1) {
+            categoriesUuids = data.map(d => d.uuid);
+            svg.selectAll('.checkbox').style("fill-opacity",0.87);
+          }
+
+          let newData = data.filter((d) => {
+              return categoriesUuids.indexOf(d.uuid) > -1;
+            })
+
+          seriesMerged = mergeData(newData);
+
+          maxValue = d3.max(seriesMerged.map(d => d.value));
+
+          let newCircles = svg.selectAll('.circles').selectAll('circle')
+          .data(seriesMerged);
+
+          newCircles
+            .attr("r", d => gridSize/2 * (d.value /maxValue))
+            .transition().duration(500);
+
+          newCircles.exit().remove();
+
+        }
+
+        function mouseover() {
+          tooltip
+            .style("z-index", "100")
+            .style("opacity", 0.9);
+        }
+
+        function mousemove(d, element) {
+          let elementRect = element.getBoundingClientRect();
+          let tooltipText = "<tr><td><b>"+ gettextCatalog.getString("Total") +
+                            "</td><td><b>"+ d.value + "</td></tr><tr><td></td></tr>"
+          d.category.forEach(function(category){
+              if (category.value) {
+                tooltipText =
+                        tooltipText +
+                        ("<tr><td>"+ category.label +
+                        "</td><td><b>"+ category.value + "</td></tr>");
+              }
+          })
+          tooltip
+            .html("<table><tbody>"+ tooltipText + "</tbody></table>")
+            .style("left", elementRect.right + 10 + "px")
+            .style("top", elementRect.top + (elementRect.height/2) - tooltip.property('clientHeight')/2 + "px")
+        }
+
+        function mouseleave() {
+          tooltip
+            .style("z-index", "-100")
+            .style("opacity", 0)
+        }
       }
       return {
           draw: draw
